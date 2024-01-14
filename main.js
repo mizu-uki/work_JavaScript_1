24e0ec77b731f0bdd996569c3fc5939ff8e61b04
mizu-uki <ueda123g@gmail.com>
2024-01-14 12:24:15 +0900

    ■以下要件を修正。 ・ストップボタンを押したあとに再スタートした際にミリ秒が0からカウントされるのを修正 （例えば0:0:4:9とかで止めてみて再スタート時にすぐに5に切り上がらないなど） ・60秒以降のカウントが0に戻ってしまうのを修正
    ■編集ファイル
    ・index.html
    ・main.js

--- main.js
+++ main.js
@@ -1,94 +1,71 @@
-// ーーースタートボタンーーー
-function startTimer(){
-    mill_intervalId = setInterval(countMillSecond, 100) 
-    second_intervalId = setInterval(countSecond, 1000)
-    minute_intervalId = setInterval(countMinute, 60000)
-    hour_intervalId = setInterval(countHour, 360000)
-
-    document.getElementById('start').disabled = true;
-    document.getElementById('stop').disabled = false;
-    document.getElementById('reset').disabled = false;
-}
-
-// mill_secondのカウント
-let elm_mill_second = document.getElementById('mill_second')
-let count_mill_second = 0
-
-function countMillSecond(){
-    if(count_mill_second < 9){
-        count_mill_second += 1
-    }else{
-        count_mill_second = 0
+$(document).ready(function(){
+    mill_second_value = 0
+    second_value = 0
+    minute_value = 0
+    hour_value = 0
+    
+    // スタートボタン
+    $("#start").click(function(){
+        IntervalId = setInterval(countTime, 100)
+        $("#start").prop('disabled', true)
+        $("#stop").prop('disabled', false)
+        $("#reset").prop('disabled', false)
+    })
+    
+    // 経過時間カウント（ミリ秒）
+    let elapsedTime = 0
+        // let elapsedTime = 100 * 10 *60 *60 - 2000
+    function countTime(){
+        elapsedTime += 100
+        console.log(elapsedTime)
+        
+        updateTime(elapsedTime)
     }
-    elm_mill_second.innerHTML = count_mill_second
-}
-
-
-// secondのカウント
-let elm_second = document.getElementById('second')
-let count_second = 0
-
-function countSecond(){
-    if(count_second < 59 ){
-        count_second += 1
-    }else{
-        count_second = 0
+    
+    function updateTime(time){
+        mill_second_value = (time % 1000) / 100
+        second_value = Math.floor(time / 1000) % 60
+        minute_value = Math.floor(time / 1000 /60) % 60
+        hour_value = Math.floor(time / 1000 / 60 / 60)
+        
+        
+        $("#mill_second").text(mill_second_value)
+        $("#second").text(second_value)
+        $("#minute").text(minute_value)
+        $("#hour").text(hour_value)
     }
-    elm_second.innerHTML = count_second
-}
+    
+    // ストップボタン
+    $("#stop").click(function(){
+        clearInterval(IntervalId)
+        
+        $("#start").prop('disabled', false)
+        $("#stop").prop('disabled', true)
+        $("#reset").prop('disabled', false)
+    })
+    
+    // リセットボタン
+    $("#reset").click(function(){
+        clearInterval(IntervalId)
+        
+        elapsedTime =  0
+        
+        mill_second_value = 0
+        second_value = 0
+        minute_value = 0
+        hour_value = 0
+        
+        $("#mill_second").text(mill_second_value)
+        $("#second").text(second_value)
+        $("#minute").text(minute_value)
+        $("#hour").text(hour_value)
+        
+        $("#start").prop('disabled', false)
+        $("#stop").prop('disabled', true)
+        $("#reset").prop('disabled', true)
+    })
+    
+    
+    
+})
 
-// minutesのカウント
-let elm_minute = document.getElementById('minute')
-let count_minute = 0
-
-function countMinute(){
-    if(count_minute < 59 ){
-        count_minute += 1
-    }else{
-        count_minute = 0
-    }
-    elm_minute.innerHTML = count_minute
-}
-
-// hourのカウント
-let elm_hour = document.getElementById('hour')
-let count_hour = 0
-
-function countHour(){
-    count_hour += 1
-    elm_hour.innerHTML = count_hour
-}
-
-// ーーーストップボタンーーー
-function stopTimer(){
-    clearInterval(mill_intervalId)
-    clearInterval(second_intervalId)
-    clearInterval(minute_intervalId)
-    clearInterval(hour_intervalId)
-
-    document.getElementById('start').disabled = false;
-    document.getElementById('stop').disabled = true;
-    document.getElementById('reset').disabled = false;
-}
-
-// ーーーリセットボタンーーー
-function resetTimer(){
-    count_mill_second = 0
-    count_second = 0
-    count_minute = 0
-    count_hour = 0
-
-    elm_mill_second.innerHTML = count_mill_second
-    elm_second.innerHTML = count_second
-    elm_minute.innerHTML = count_minute
-    elm_hour.innerHTML = count_hour
-
-    document.getElementById('start').disabled = false;
-    document.getElementById('stop').disabled = true;
-    document.getElementById('reset').disabled = true;
-
-    clearInterval(mill_intervalId)
-    clearInterval(second_intervalId)
-    clearInterval(minute_intervalId)
-    clearInterval(hour_intervalId)
-}
